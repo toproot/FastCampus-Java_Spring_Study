d497459efd2f0b0a330b40d04e0c1ab35ffd21b7
21379 <21379@KRK4OFLTP00644.kr.ds.amkor.com>
2025-01-10 21:56:13 +0900

    create Obj Class

--- Java_Spring-Study/src/ch01/Student.java
+++ Java_Spring-Study/src/ch01/Student.java
@@ -1,5 +1,8 @@
 package ch01;
 
 public class Student {
-
+	int studentId;
+	String studentName;
+	int majorCode;
+	int grade;
 }
---
+++ Java_Spring-Study/src/ch01/UserInfo.java
@@ -0,0 +1,9 @@
+package ch01;
+
+public class UserInfo {
+	String userId;
+	String userPassWord;
+	String userName;
+	String userAddress;
+	int phoneNumber;
+}
